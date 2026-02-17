0d686518bcbb90fc3eda4aa822ac30cb2dd33df7
Nikita Mandhan <nikita@Nikitas-MacBook-Pro.local>
2026-02-16 22:48:59 -0800

    Updated dag

--- dags/youtube_analytics_dag.py
+++ dags/youtube_analytics_dag.py
@@ -1,6 +1,8 @@
 from datetime import datetime, timedelta
 from airflow import DAG
 from airflow.operators.bash import BashOperator
+from airflow.operators.python import BranchPythonOperator
+from airflow.utils.trigger_rule import TriggerRule
 
 default_args = {
     'owner': 'airflow',
@@ -15,19 +17,40 @@ default_args = {
 with DAG(
     'youtube_analytics_dbt',
     default_args=default_args,
-    description='Run dbt models for YouTube analytics',
+    description='Run dbt models for YouTube analytics with data quality checks',
     schedule_interval='@daily',
     catchup=False,
 ) as dag:
 
-    dbt_run = BashOperator(
-        task_id='dbt_run',
-        bash_command='cd /Users/nikita/Downloads/youtube-snowflake-dbt-pipeline/youtube_analytics && dbt run',
+    dbt_run_staging = BashOperator(
+        task_id='dbt_run_staging',
+        bash_command='cd /Users/nikita/Downloads/youtube-snowflake-dbt-pipeline/youtube_analytics && dbt run --select staging',
     )
 
-    dbt_test = BashOperator(
-        task_id='dbt_test',
-        bash_command='cd /Users/nikita/Downloads/youtube-snowflake-dbt-pipeline/youtube_analytics && dbt test',
+    dbt_test_staging = BashOperator(
+        task_id='dbt_test_staging',
+        bash_command='cd /Users/nikita/Downloads/youtube-snowflake-dbt-pipeline/youtube_analytics && dbt test --select staging',
     )
 
-    dbt_run >> dbt_test
+    dbt_run_intermediate = BashOperator(
+        task_id='dbt_run_intermediate',
+        bash_command='cd /Users/nikita/Downloads/youtube-snowflake-dbt-pipeline/youtube_analytics && dbt run --select intermediate',
+    )
+
+    dbt_run_marts = BashOperator(
+        task_id='dbt_run_marts',
+        bash_command='cd /Users/nikita/Downloads/youtube-snowflake-dbt-pipeline/youtube_analytics && dbt run --select marts',
+    )
+
+    dbt_test_marts = BashOperator(
+        task_id='dbt_test_marts',
+        bash_command='cd /Users/nikita/Downloads/youtube-snowflake-dbt-pipeline/youtube_analytics && dbt test --select marts',
+    )
+
+    data_quality_check = BashOperator(
+        task_id='data_quality_check',
+        bash_command='cd /Users/nikita/Downloads/youtube-snowflake-dbt-pipeline/youtube_analytics && dbt test --select data_quality_metrics',
+        trigger_rule=TriggerRule.ALL_DONE,
+    )
+
+    dbt_run_staging >> dbt_test_staging >> dbt_run_intermediate >> dbt_run_marts >> dbt_test_marts >> data_quality_check
